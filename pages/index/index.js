b8c09001af615b7c6ad50f6173a4caa224bef2c3
james <1071714548@qq.com>
2019-06-28 20:04:54 +0800

    小程序页面的生命周期

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,5 +17,60 @@ Page({
   },
   swiperChange() {
     // console.log(111);
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    console.log('生命周期函数--监听页面加载');
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    console.log('生命周期函数--监听页面初次渲染完成');
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    console.log('生命周期函数--监听页面显示');
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    console.log('生命周期函数--监听页面隐藏');
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    console.log('生命周期函数--监听页面卸载');
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    console.log('监听用户下拉动作');
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+    console.log('页面上拉触底事件的处理函数');
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+    console.log('====')
   }
 })
